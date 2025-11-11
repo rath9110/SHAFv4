259406145737cd86adcd87838528f28af035ce43
Rasmus Thunberg <rath701@gmail.com>
2025-11-11 20:56:48 +0100

    updated base url'

--- popup.js
+++ popup.js
@@ -123,7 +123,7 @@ async function fetchRelatedProducts(searchTerm, pageKey) {
     searchResults.innerHTML = `<p> Searching for "${searchTerm}"...</p>`;
 
     try {
-        const response = await fetch(`http://127.0.0.1:8000/related-products?product_name=${encodeURIComponent(searchTerm)}`, {
+        const response = await fetch(`https://sha-fv4.vercel.app/related-products?product_name=${encodeURIComponent(searchTerm)}`, {
             method: "GET",
             headers: { "Content-Type": "application/json" }
         });
