9b3a955a7ab93d5f2811b2866908f84ecdce5836
Rasmus Thunberg <rath701@gmail.com>
2025-11-30 18:56:16 +0100

    cors

--- backend/fetching_service.py
+++ backend/fetching_service.py
@@ -44,8 +44,8 @@ async def startup_event():
 # Allow CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://94.255.240.88", "https://94.255.240.88"],  # Allow specific IP
-    allow_origin_regex="chrome-extension://.*",  # Allow all Chrome extensions
+    allow_origins=["*"],  # Allow specific IP
+    allow_origin_regex="*",  # Allow all Chrome extensions
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
